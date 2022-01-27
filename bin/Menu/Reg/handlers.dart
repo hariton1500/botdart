df32f97a2686a1ce64ac5e9013f61fc66ac52eb8
hariton1500 <57896518+hariton1500@users.noreply.github.com>
2022-01-27 23:09:25 +0300

    12

--- bin/Menu/Reg/handlers.dart
+++ bin/Menu/Reg/handlers.dart
@@ -184,3 +184,185 @@ regHandler(String text, TeleDart bot, int chatId, Abon abon,
   }
   print('out--> ' + abon.toString());
 }
+
+regHandler2(String text, Bot bot, int chatId, Abon abon,
+    Map<String, Map<String, dynamic>> users) async {
+  switch (abon.menuLevel) {
+    case 'top':
+      switch (text) {
+        case 'авторизация':
+          bot.sendMessage(chatId, mess['askId&Phone']!,
+              reply_markup: markups['reg']);
+          abon.menuLevel = 'askIdPhoneBack';
+          abon.statusReg = false;
+          break;
+        case 'учетные записи':
+          abon.menuLevel = 'accs';
+          for (var guid in abon.guids!) {
+            var res = await abon.getInfo(chatId, guid);
+            if (!res['error']) {
+              users[guid] = res['message']['userinfo'];
+              bot.sendMessage(
+                  chatId, '${users[guid]!['id']} - информация загружена');
+            }
+          }
+          bot.sendMessage(chatId, mess['accs']! + menu['accs']!,
+              reply_markup: ReplyKeyboardMarkup(keyboard: [
+                [btnBrief, btnFull],
+                uidBtns(abon.guids!
+                    .map((guid) => int.parse(users[guid]!['id'].toString()))
+                    .toList()),
+                [btnBack]
+              ]));
+          break;
+        case 'справочник абонента':
+          bot.sendMessage(chatId,
+              'Добро пожаловать в справочную абонента. Выбор тем внизу экрана:',
+              reply_markup: markups['help']);
+          abon.menuLevel = 'help';
+          break;
+        default:
+          bot.sendMessage(chatId, mess['topIn']! + menu['topIn']!,
+              reply_markup: markups['topIn']);
+      }
+      break;
+    case 'accs':
+      switch (text) {
+        case 'показать кратко':
+          print('показать кратко');
+          bot.sendMessage(chatId, abon.showUsersInfo(true, users));
+          break;
+        case 'показать подробно':
+          bot.sendMessage(chatId, abon.showUsersInfo(false, users));
+          break;
+        case 'назад':
+          abon.menuLevel = 'top';
+          bot.sendMessage(chatId, mess['topIn']! + menu['topIn']!,
+              reply_markup: markups['topIn']);
+          break;
+        default:
+          if (int.tryParse(text) != null) {
+            print('text is digit');
+            int id = int.parse(text);
+            List<String> list = List.from(
+                abon.guids!.map((guid) => users[guid]!['id']).toList());
+            if (list.contains(id.toString())) {
+              print('$id found in ${abon.showUsersInfo(true, users)}');
+              abon.selectedId = id;
+              abon.selectedGuid = abon.guids![list.indexOf(id.toString())];
+              bot.sendMessage(chatId,
+                  '${mess['id']}\n\n${abon.showUserInfo(users)}\n\n${menu['id']}',
+                  reply_markup: markups['id']);
+              abon.menuLevel = 'id';
+            } else {
+              print('$id not found in ${abon.showUsersInfo(true, users)}');
+            }
+          } else {
+            print('text is not digit');
+          }
+      }
+      break;
+    case 'id':
+      switch (text) {
+        case 'сообщение с службу поддержки':
+          bot.sendMessage(
+              chatId, 'Введите текст сообщения для службы поддержки:',
+              reply_markup: ReplyKeyboardMarkup(keyboard: [
+                [btnBack]
+              ], resize_keyboard: true));
+          abon.menuLevel = 'id_mess';
+          break;
+        case 'пополнить баланс':
+          bot.sendMessage(chatId,
+              'Введите сумму платежа, учтите, что платежная система берет комиссию 6%',
+              reply_markup: ReplyKeyboardMarkup(keyboard: [
+                [btnBack]
+              ], resize_keyboard: true));
+          abon.menuLevel = 'id_sum';
+          break;
+        case 'назад':
+          abon.menuLevel = 'accs';
+          bot.sendMessage(chatId, mess['accs']! + menu['accs']!,
+              reply_markup: ReplyKeyboardMarkup(keyboard: [
+                [btnBrief, btnFull],
+                uidBtns(abon.guids!
+                    .map((guid) => int.parse(users[guid]!['id'].toString()))
+                    .toList()),
+                [btnBack]
+              ], resize_keyboard: true));
+          break;
+        default:
+      }
+      break;
+    case 'id_mess':
+      switch (text) {
+        case 'назад':
+          abon.menuLevel = 'id';
+          bot.sendMessage(chatId,
+              '${mess['id']}\n\n${abon.showUserInfo(users)}\n\n${menu['id']}',
+              reply_markup: markups['id']);
+          break;
+        default:
+          var res = await abon.sendRemont(abon.selectedGuid!, chatId, text);
+          if (!res['error']) {
+            bot.sendMessage(chatId, res['message']);
+          }
+          bot.sendMessage(chatId,
+              '${mess['id']}\n\n${abon.showUserInfo(users)}\n\n${menu['id']}',
+              reply_markup: markups['id']);
+          abon.menuLevel = 'id';
+      }
+      break;
+    case 'id_sum':
+      switch (text) {
+        case 'назад':
+          abon.menuLevel = 'id';
+          bot.sendMessage(chatId,
+              '${mess['id']}\n\n${abon.showUserInfo(users)}\n\n${menu['id']}',
+              reply_markup: markups['id']);
+          break;
+        default:
+          if (int.tryParse(text) != null) {
+            bot.sendMessage(chatId,
+                'Для пополнения баланса учетной записи ${abon.selectedId} перейдите по ссылке:');
+            var res = await abon.getPaymentId(abon.selectedGuid!, chatId);
+            String url =
+                'https://paymaster.ru/payment/init?LMI_MERCHANT_ID=95005d6e-a21d-492a-a4c5-c39773020dd3&LMI_PAYMENT_AMOUNT=' +
+                    text.toString() +
+                    '&LMI_CURRENCY=RUB&LMI_PAYMENT_NO=' +
+                    res['message']['payment_id'].toString() +
+                    '&LMI_PAYMENT_DESC=%D0%9F%D0%BE%D0%BF%D0%BE%D0%BB%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5%20EvpaNet%20ID%20' +
+                    abon.selectedId.toString();
+            bot.sendMessage(chatId, '[ПОПОЛНИТЬ БАЛАНС]($url)',
+                parse_mode: 'markdown',
+                reply_markup: ReplyKeyboardMarkup(keyboard: [
+                  [btnBack]
+                ]));
+            abon.menuLevel = 'id';
+          }
+      }
+      break;
+    case 'help':
+      switch (text) {
+        case 'выбор WiFi роутера':
+          bot.sendMessage(chatId, mess['choose_router']!);
+          break;
+        case 'список услуг компании EvpaNet':
+          bot.sendMessage(chatId, mess['services']!);
+          break;
+        case 'способы оплаты':
+          bot.sendMessage(chatId,
+              'https://evpanet.com/users/payments.html?tmpl=component&print=1');
+          break;
+        case 'назад':
+          abon.menuLevel = 'top';
+          bot.sendMessage(chatId, mess['topIn']! + menu['topIn']!,
+              reply_markup: markups['topIn']);
+          break;
+        default:
+      }
+      break;
+    default:
+  }
+  print('out--> ' + abon.toString());
+}
--- bin/Menu/UnReg/handlers.dart
+++ bin/Menu/UnReg/handlers.dart
@@ -12,10 +12,11 @@ unregHandler(String text, TeleDart bot, int chatId, Abon abon) async {
           abon.menuLevel = 'askIdPhoneBack';
           break;
         case 'описание':
-          bot.sendMessage(chatId, mess['itCan']! + mess['about']!, reply_markup: markups['topNotIn']);
+          bot.sendMessage(chatId, mess['itCan']! + mess['about']!,
+              reply_markup: markups['topNotIn']);
           break;
         default:
-          //bot.sendMessage(chatId, menu[abon.statusReg! ? 'topIn' : 'topNotIn']!, reply_markup: markups[abon.statusReg! ? 'topIn' : 'topNotIn']);
+        //bot.sendMessage(chatId, menu[abon.statusReg! ? 'topIn' : 'topNotIn']!, reply_markup: markups[abon.statusReg! ? 'topIn' : 'topNotIn']);
       }
       break;
     case 'askIdPhoneBack':
@@ -34,7 +35,12 @@ unregHandler(String text, TeleDart bot, int chatId, Abon abon) async {
         case 'назад':
           abon.statusReg = abon.guids!.isNotEmpty;
           abon.menuLevel = 'top';
-          bot.sendMessage(chatId, mess['start']! + mess['itCan']! + mess['about']! + menu[abon.statusReg! ? 'topIn' : 'topNotIn']!,
+          bot.sendMessage(
+              chatId,
+              mess['start']! +
+                  mess['itCan']! +
+                  mess['about']! +
+                  menu[abon.statusReg! ? 'topIn' : 'topNotIn']!,
               reply_markup: markups[abon.statusReg! ? 'topIn' : 'topNotIn']);
           break;
         default:
@@ -59,11 +65,22 @@ unregHandler(String text, TeleDart bot, int chatId, Abon abon) async {
                 reply_markup: markups['topIn']);
           } else {
             abon.menuLevel = 'askIdPhoneBack';
-            bot.sendMessage(chatId, 'ИНФО: ' + resp['message'] + '\nПроверьте правильность введенных данных:\n' +  '\nID: ${abon.uid}\nНомер телефона: ${abon.phone}' '\n\n' + menu['reg']!,
+            bot.sendMessage(
+                chatId,
+                'ИНФО: ' +
+                    resp['message'] +
+                    '\nПроверьте правильность введенных данных:\n' +
+                    '\nID: ${abon.uid}\nНомер телефона: ${abon.phone}' '\n\n' +
+                    menu['reg']!,
                 reply_markup: markups['reg']);
           }
         } else {
-          bot.sendMessage(chatId, 'ID: ${abon.uid}\nНомер телефона: ${abon.phone != null ? abon.phone! : 'еще не указан'}' '\n'  + menu['reg']!, reply_markup: markups['reg']);
+          bot.sendMessage(
+              chatId,
+              'ID: ${abon.uid}\nНомер телефона: ${abon.phone != null ? abon.phone! : 'еще не указан'}'
+                      '\n' +
+                  menu['reg']!,
+              reply_markup: markups['reg']);
         }
       } else {
         bot.sendMessage(chatId, mess['wrongId']!);
@@ -88,11 +105,151 @@ unregHandler(String text, TeleDart bot, int chatId, Abon abon) async {
                 reply_markup: markups['topIn']);
           } else {
             abon.menuLevel = 'askIdPhoneBack';
-            bot.sendMessage(chatId, 'ИНФО: ' + resp['message'] + '\nПроверьте правильность введенных данных:\n' +  '\nID: ${abon.uid}\nНомер телефона: ${abon.phone}' '\n\n' + menu['reg']!,
+            bot.sendMessage(
+                chatId,
+                'ИНФО: ' +
+                    resp['message'] +
+                    '\nПроверьте правильность введенных данных:\n' +
+                    '\nID: ${abon.uid}\nНомер телефона: ${abon.phone}' '\n\n' +
+                    menu['reg']!,
                 reply_markup: markups['reg']);
           }
         } else {
-          bot.sendMessage(chatId, 'ID: ${abon.uid}\nНомер телефона: ${abon.phone != null ? abon.phone! : 'еще не указан'}' '\n' + menu['reg']!, reply_markup: markups['reg']);
+          bot.sendMessage(
+              chatId,
+              'ID: ${abon.uid}\nНомер телефона: ${abon.phone != null ? abon.phone! : 'еще не указан'}'
+                      '\n' +
+                  menu['reg']!,
+              reply_markup: markups['reg']);
+        }
+      } else {
+        bot.sendMessage(chatId, mess['wrongPhone']!);
+      }
+      break;
+    default:
+  }
+  print('out--> ' + abon.toString());
+}
+
+unregHandler2(String text, Bot bot, int chatId, Abon abon) async {
+  switch (abon.menuLevel) {
+    case 'top':
+      switch (text) {
+        case 'авторизация':
+          bot.sendMessage(chatId, mess['askId&Phone']! + menu['reg']!,
+              reply_markup: markups['reg']);
+          abon.menuLevel = 'askIdPhoneBack';
+          break;
+        case 'описание':
+          bot.sendMessage(chatId, mess['itCan']! + mess['about']!,
+              reply_markup: markups['topNotIn']);
+          break;
+        default:
+        //bot.sendMessage(chatId, menu[abon.statusReg! ? 'topIn' : 'topNotIn']!, reply_markup: markups[abon.statusReg! ? 'topIn' : 'topNotIn']);
+      }
+      break;
+    case 'askIdPhoneBack':
+      switch (text) {
+        case 'ввести id':
+          bot.sendMessage(chatId, 'Введите ID:',
+              reply_markup: markups['empty']);
+          abon.menuLevel = 'enterId';
+          break;
+        case 'ввести номер телефона':
+          bot.sendMessage(chatId,
+              'Введите номер телефона в формате +########### без пробелов и скобок:',
+              reply_markup: markups['empty']);
+          abon.menuLevel = 'enterPhone';
+          break;
+        case 'назад':
+          abon.statusReg = abon.guids!.isNotEmpty;
+          abon.menuLevel = 'top';
+          bot.sendMessage(
+              chatId,
+              mess['start']! +
+                  mess['itCan']! +
+                  mess['about']! +
+                  menu[abon.statusReg! ? 'topIn' : 'topNotIn']!,
+              reply_markup: markups[abon.statusReg! ? 'topIn' : 'topNotIn']);
+          break;
+        default:
+      }
+      break;
+    case 'enterId':
+      if (int.tryParse(text) != null) {
+        abon.uid = int.parse(text);
+        abon.menuLevel = 'askIdPhoneBack';
+        bot.sendMessage(chatId, 'ID сохранен');
+        //sleep(Duration(seconds: 1));
+        //bot.sendMessage(chatId, mess['askId&Phone']!, reply_markup: markups['reg']);
+        if (abon.phone != null && abon.phone!.isNotEmpty) {
+          var resp = await abon.register();
+          if (!resp['error']) {
+            abon.guids = List.from(resp['message']['guids']);
+            abon.saveGuids();
+            abon.menuLevel = 'top';
+            abon.statusReg = true;
+            bot.sendMessage(chatId, 'Авторизация прошла успешно');
+            bot.sendMessage(chatId, mess['topIn']! + menu['topIn']!,
+                reply_markup: markups['topIn']);
+          } else {
+            abon.menuLevel = 'askIdPhoneBack';
+            bot.sendMessage(
+                chatId,
+                'ИНФО: ' +
+                    resp['message'] +
+                    '\nПроверьте правильность введенных данных:\n' +
+                    '\nID: ${abon.uid}\nНомер телефона: ${abon.phone}' '\n\n' +
+                    menu['reg']!,
+                reply_markup: markups['reg']);
+          }
+        } else {
+          bot.sendMessage(
+              chatId,
+              'ID: ${abon.uid}\nНомер телефона: ${abon.phone != null ? abon.phone! : 'еще не указан'}'
+                      '\n' +
+                  menu['reg']!,
+              reply_markup: markups['reg']);
+        }
+      } else {
+        bot.sendMessage(chatId, mess['wrongId']!);
+      }
+      break;
+    case 'enterPhone':
+      if (isPhone(text)) {
+        abon.phone = text;
+        abon.menuLevel = 'askIdPhoneBack';
+        //sleep(Duration(seconds: 1));
+        bot.sendMessage(chatId, 'Номер телефона сохранен');
+        //bot.sendMessage(chatId, mess['askId&Phone']!, reply_markup: markups['reg']);
+        if (abon.uid != null && abon.uid! > 0) {
+          var resp = await abon.register();
+          if (!resp['error']) {
+            abon.guids = List.from(resp['message']['guids']);
+            abon.saveGuids();
+            abon.menuLevel = 'top';
+            abon.statusReg = true;
+            bot.sendMessage(chatId, 'Авторизация прошла успешно');
+            bot.sendMessage(chatId, mess['topIn']! + menu['topIn']!,
+                reply_markup: markups['topIn']);
+          } else {
+            abon.menuLevel = 'askIdPhoneBack';
+            bot.sendMessage(
+                chatId,
+                'ИНФО: ' +
+                    resp['message'] +
+                    '\nПроверьте правильность введенных данных:\n' +
+                    '\nID: ${abon.uid}\nНомер телефона: ${abon.phone}' '\n\n' +
+                    menu['reg']!,
+                reply_markup: markups['reg']);
+          }
+        } else {
+          bot.sendMessage(
+              chatId,
+              'ID: ${abon.uid}\nНомер телефона: ${abon.phone != null ? abon.phone! : 'еще не указан'}'
+                      '\n' +
+                  menu['reg']!,
+              reply_markup: markups['reg']);
         }
       } else {
         bot.sendMessage(chatId, mess['wrongPhone']!);
